bf9eda07cade30f256b8cc6d95b602c14c0e2c52
bsomers42 <77967616+bsomers42@users.noreply.github.com>
2021-12-01 13:34:06 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/14 18:51:36 by bsomers       #+#    #+#                 */
-/*   Updated: 2021/04/14 18:56:45 by bsomers       ########   odam.nl         */
+/*   Updated: 2021/12/01 12:34:37 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,18 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include "get_next_line.h"
 
-int	get_next_line(int fd, char **line);
+char  	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
