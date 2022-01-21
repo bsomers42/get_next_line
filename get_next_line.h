a74ac9b3e390ea2fe0d999d4d698c030985732eb
bsomers42 <77967616+bsomers42@users.noreply.github.com>
2022-01-21 14:17:07 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/14 18:51:36 by bsomers       #+#    #+#                 */
-/*   Updated: 2021/12/01 12:34:37 by bsomers       ########   odam.nl         */
+/*   Updated: 2021/12/17 17:07:36 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,25 @@
 
 # define GET_NEXT_LINE_H
 
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <string.h>
-#include "get_next_line.h"
+# include <limits.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "get_next_line.h"
 
-char  	*get_next_line(int fd);
+typedef struct s_info
+{
+	int		ret;
+	int		n;
+	char	*nextline;
+	int		a;
+	int		flag;
+}		t_info;
+
+char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len, t_info *stat);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+char	*joinstr(char *s1, char c, char *sdef);
 
 #endif
