3ee4e329eeab79bf00ba980a0b4d2b2ed1779e49
bsomers42 <77967616+bsomers42@users.noreply.github.com>
2022-01-21 14:17:33 +0100

    Final version uploaded

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: bsomers <bsomers@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/14 18:57:19 by bsomers       #+#    #+#                 */
-/*   Updated: 2021/11/30 20:03:37 by bsomers       ########   odam.nl         */
+/*   Updated: 2021/12/17 17:07:37 by bsomers       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,61 +20,6 @@ size_t	ft_strlen(const char *s)
 	while (s[i] != '\0')
 		i++;
 	return (i);
-}
-
-static char	*joinstr(char *s1, char *s2, char *sdef)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
-	{
-		sdef[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		sdef[i] = s2[j];
-		i++;
-		j++;
-	}
-	sdef[i] = '\0';
-	return (sdef);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*sdef;
-	int		i;
-	int		j;
-
-	i = 0;
-	if (s2 == NULL)
-		return (0);
-	if (s1)
-		i = ft_strlen(s1);
-	j = ft_strlen(s2);
-	if (s1 == NULL)
-	{
-		sdef = malloc((j + 1) * (sizeof(char)));
-		if (sdef == NULL)
-			return (NULL);
-		while (i <= j)//adjusted
-		{
-			sdef[i] = s2[i];
-			i++;
-		}
-		//sdef[i] = '\0';
-		return (sdef);
-	}
-	sdef = malloc((i + j + 1) * (sizeof(char)));
-	if (sdef == NULL)
-		return (NULL);
-	sdef = joinstr(s1, s2, sdef);
-	free(s1);
-	return (sdef);
 }
 
 static	int	ft_overlap(const void *dst, const void *src, size_t len)
@@ -112,4 +57,50 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		}
 	}
 	return (dst);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len, t_info *stat)
+{
+	char			*sub;
+	size_t			count;
+	size_t			strl;
+
+	count = 0;
+	if (s == NULL)
+		return (0);
+	strl = ft_strlen(s);
+	if (strl == 0)
+		return (NULL);
+	if ((start + len) > strl)
+		len = strl - start;
+	sub = malloc((len + 1) * (sizeof(unsigned char)));
+	if (sub == NULL)
+		stat->flag = 1;
+	if (stat->flag == 1)
+		return (NULL);
+	while (count < len && s[start] != '\0')
+	{
+		sub[count] = s[start];
+		start++;
+		count++;
+	}
+	sub[count] = '\0';
+	return (sub);
+}
+
+char	*joinstr(char *s1, char c, char *sdef)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		sdef[i] = s1[i];
+		i++;
+	}
+	sdef[i] = c;
+	sdef[i + 1] = '\0';
+	return (sdef);
 }
